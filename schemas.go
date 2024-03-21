b451a9158da8dce23c6c2b3dec5c4d6754c24495
vizzyolog <vizzyolog@gmail.com>
2024-03-21 10:40:00 -0300

    embed schema

--- schemas.go
+++ schemas.go
@@ -2,23 +2,7 @@ package popugschema
 
 import (
 	_ "embed"
-
-	"github.com/dangkaka/go-kafka-avro"
 )
 
 //go:embed user_schema
-var userSchema string
-
-var kafkaServers = []string{"localhost:9092"}
-var schemaRegistryServers = []string{"http://localhost:8081"}
-var topic = "users"
-
-func NewUserProducer() (*kafka.AvroProducer, error) {
-	return kafka.NewAvroProducer(kafkaServers, schemaRegistryServers)
-}
-
-func sendUserUpdate(producer *kafka.AvroProducer, schema string) {
-	value := `{
-		
-	}`
-}
+var UserSchema string
